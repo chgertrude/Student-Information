b2df4f38e0fd4c72762b1bcbde725ec96de8f2f5
chgrtrd11 <131791000+chgrtrd11@users.noreply.github.com>
2023-09-05 22:11:07 +0800

    Add files via upload

---
+++ src/File/Print.java
@@ -0,0 +1,207 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package File;
+
+
+
+/**
+ *
+ * @author Gertrude Anne Chua
+ * Final Frame- Last Class/Frame to be run.
+ */
+public class Print extends javax.swing.JFrame {
+
+    /**
+     * Creates new form Print
+     */
+    public Print() {
+        initComponents();
+ 
+    }
+
+  
+    @SuppressWarnings("unchecked")
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    private void initComponents() {
+
+        jPanel1 = new javax.swing.JPanel();
+        fullName = new javax.swing.JLabel();
+        studentNum = new javax.swing.JLabel();
+        course = new javax.swing.JLabel();
+        presentAdd = new javax.swing.JLabel();
+        dateOfBirth = new javax.swing.JLabel();
+        agePrint = new javax.swing.JLabel();
+        placeOfBirth = new javax.swing.JLabel();
+        lastSchoolPrint = new javax.swing.JLabel();
+        honorsReceived = new javax.swing.JLabel();
+        discountPrint = new javax.swing.JLabel();
+        lecturePrint = new javax.swing.JLabel();
+        labPrint = new javax.swing.JLabel();
+        totalTuitionPrint = new javax.swing.JLabel();
+        missFeePrint = new javax.swing.JLabel();
+        TotalTuitionTobePaid = new javax.swing.JLabel();
+        jLabel1 = new javax.swing.JLabel();
+
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setResizable(false);
+        setSize(new java.awt.Dimension(1021, 751));
+
+        jPanel1.setPreferredSize(new java.awt.Dimension(1201, 751));
+        jPanel1.setLayout(null);
+
+        fullName.setFont(new java.awt.Font("Tahoma", 0, 18)); // NOI18N
+        fullName.setForeground(new java.awt.Color(51, 51, 51));
+        jPanel1.add(fullName);
+        fullName.setBounds(310, 160, 530, 30);
+
+        studentNum.setFont(new java.awt.Font("Tahoma", 0, 18)); // NOI18N
+        studentNum.setForeground(new java.awt.Color(51, 51, 51));
+        jPanel1.add(studentNum);
+        studentNum.setBounds(310, 200, 150, 30);
+
+        course.setFont(new java.awt.Font("Tahoma", 0, 17)); // NOI18N
+        course.setForeground(new java.awt.Color(51, 51, 51));
+        jPanel1.add(course);
+        course.setBounds(560, 200, 280, 30);
+
+        presentAdd.setFont(new java.awt.Font("Tahoma", 0, 18)); // NOI18N
+        presentAdd.setForeground(new java.awt.Color(51, 51, 51));
+        jPanel1.add(presentAdd);
+        presentAdd.setBounds(310, 240, 520, 40);
+
+        dateOfBirth.setFont(new java.awt.Font("Tahoma", 0, 18)); // NOI18N
+        dateOfBirth.setForeground(new java.awt.Color(51, 51, 51));
+        jPanel1.add(dateOfBirth);
+        dateOfBirth.setBounds(310, 290, 150, 30);
+
+        agePrint.setFont(new java.awt.Font("Tahoma", 0, 18)); // NOI18N
+        agePrint.setForeground(new java.awt.Color(51, 51, 51));
+        jPanel1.add(agePrint);
+        agePrint.setBounds(550, 280, 80, 40);
+
+        placeOfBirth.setFont(new java.awt.Font("Tahoma", 0, 18)); // NOI18N
+        placeOfBirth.setForeground(new java.awt.Color(51, 51, 51));
+        jPanel1.add(placeOfBirth);
+        placeOfBirth.setBounds(380, 320, 460, 40);
+
+        lastSchoolPrint.setFont(new java.awt.Font("Tahoma", 0, 18)); // NOI18N
+        lastSchoolPrint.setForeground(new java.awt.Color(51, 51, 51));
+        jPanel1.add(lastSchoolPrint);
+        lastSchoolPrint.setBounds(380, 370, 450, 40);
+
+        honorsReceived.setFont(new java.awt.Font("Tahoma", 0, 18)); // NOI18N
+        honorsReceived.setForeground(new java.awt.Color(51, 51, 51));
+        jPanel1.add(honorsReceived);
+        honorsReceived.setBounds(380, 410, 250, 40);
+
+        discountPrint.setFont(new java.awt.Font("Tahoma", 0, 18)); // NOI18N
+        discountPrint.setForeground(new java.awt.Color(51, 51, 51));
+        jPanel1.add(discountPrint);
+        discountPrint.setBounds(770, 410, 60, 40);
+
+        lecturePrint.setFont(new java.awt.Font("Tahoma", 0, 18)); // NOI18N
+        lecturePrint.setForeground(new java.awt.Color(51, 51, 51));
+        jPanel1.add(lecturePrint);
+        lecturePrint.setBounds(570, 460, 70, 30);
+
+        labPrint.setFont(new java.awt.Font("Tahoma", 0, 18)); // NOI18N
+        labPrint.setForeground(new java.awt.Color(51, 51, 51));
+        jPanel1.add(labPrint);
+        labPrint.setBounds(790, 460, 40, 30);
+
+        totalTuitionPrint.setFont(new java.awt.Font("Tahoma", 0, 18)); // NOI18N
+        jPanel1.add(totalTuitionPrint);
+        totalTuitionPrint.setBounds(360, 530, 100, 40);
+
+        missFeePrint.setFont(new java.awt.Font("Tahoma", 0, 18)); // NOI18N
+        missFeePrint.setForeground(new java.awt.Color(51, 51, 51));
+        jPanel1.add(missFeePrint);
+        missFeePrint.setBounds(710, 530, 120, 40);
+
+        TotalTuitionTobePaid.setFont(new java.awt.Font("Tahoma", 0, 18)); // NOI18N
+        TotalTuitionTobePaid.setForeground(new java.awt.Color(51, 51, 51));
+        jPanel1.add(TotalTuitionTobePaid);
+        TotalTuitionTobePaid.setBounds(430, 570, 160, 40);
+
+        jLabel1.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Image/Print.jpg"))); // NOI18N
+        jLabel1.setText("jLabel1");
+        jPanel1.add(jLabel1);
+        jLabel1.setBounds(0, 0, 960, 720);
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
+        getContentPane().setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, 958, javax.swing.GroupLayout.PREFERRED_SIZE)
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, 722, Short.MAX_VALUE)
+                .addGap(0, 0, Short.MAX_VALUE))
+        );
+
+        pack();
+        setLocationRelativeTo(null);
+    }// </editor-fold>//GEN-END:initComponents
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String args[]) {
+        /* Set the Nimbus look and feel */
+        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
+        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
+         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
+         */
+        try {
+            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+                if ("Nimbus".equals(info.getName())) {
+                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+        } catch (ClassNotFoundException ex) {
+            java.util.logging.Logger.getLogger(Print.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (InstantiationException ex) {
+            java.util.logging.Logger.getLogger(Print.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (IllegalAccessException ex) {
+            java.util.logging.Logger.getLogger(Print.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
+            java.util.logging.Logger.getLogger(Print.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        }
+        //</editor-fold>
+
+        /* Create and display the form */
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new Print().setVisible(true);
+            }
+        });
+        
+        
+    }
+
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    public static javax.swing.JLabel TotalTuitionTobePaid;
+    public static javax.swing.JLabel agePrint;
+    public static javax.swing.JLabel course;
+    public static javax.swing.JLabel dateOfBirth;
+    public static javax.swing.JLabel discountPrint;
+    public static javax.swing.JLabel fullName;
+    public static javax.swing.JLabel honorsReceived;
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JPanel jPanel1;
+    public static javax.swing.JLabel labPrint;
+    public static javax.swing.JLabel lastSchoolPrint;
+    public static javax.swing.JLabel lecturePrint;
+    public static javax.swing.JLabel missFeePrint;
+    public static javax.swing.JLabel placeOfBirth;
+    public static javax.swing.JLabel presentAdd;
+    public static javax.swing.JLabel studentNum;
+    public static javax.swing.JLabel totalTuitionPrint;
+    // End of variables declaration//GEN-END:variables
+}
